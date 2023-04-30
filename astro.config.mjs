a7ee4ec57de13fd65ff35adfe390a64e3e3b1bd7
Max Girkens <maxgirkens@gmail.com>
2023-04-30 11:22:50 +0200

    xml sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import mdx from '@astrojs/mdx'
 
-import mdx from "@astrojs/mdx";
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()]
-});
+  site: 'https://greatbassexercises.com',
+  integrations: [react(), mdx(), sitemap()],
+})
