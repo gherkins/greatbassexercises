374c14ffcc458d934f3910971a8a710dcf0ac388
Max Girkens <maxgirkens@gmail.com>
2024-05-09 12:47:55 +0200

    homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://greatbassexercises.com',
+  site: 'https://datenkatze.de/greatbassexercises.com',
   integrations: [react(), mdx(), sitemap()],
 })
